f4bb1cc1730f0de47d4f29835c2c94250cf9e9fd
Akatama <jimmyl@pacbell.net>
2025-05-26 23:06:02 -0700

    Trying to dockerize

---
+++ Dockerfile
@@ -0,0 +1,52 @@
+FROM python:3.13-slim AS base
+
+RUN mkdir /app
+
+WORKDIR /app
+
+ENV PYTHONUNBUFFERED=1 \
+  PYTHONDONTWRITEBYTECODE=1
+
+FROM base AS build
+
+RUN apt-get update && apt-get install -y --no-install-recommends \
+  build-essential \
+  && rm -rf /var/lib/apt/lists/*
+
+COPY --from=ghcr.io/astral-sh/uv:0.5.2 /uv /bin/uv
+
+# UV_COMPILE_BYTECODE=1 is an important startup time optimization
+ENV UV_COMPILE_BYTECODE=1 UV_LINK_MODE=copy
+
+WORKDIR /app
+
+COPY uv.lock pyproject.toml ./
+RUN --mount=type=cache,target=/root/.cache/uv \
+  uv sync --frozen --no-install-project --no-dev
+
+COPY . .
+RUN --mount=type=cache,target=/root/.cache/uv \
+  uv sync --frozen --no-dev
+
+FROM base AS runtime
+
+RUN addgroup --gid 1001 --system nonroot && \
+  adduser --no-create-home --shell /bin/false \
+  --disabled-password --uid 1001 --system --group nonroot
+
+RUN chown nonroot:nonroot /app
+
+USER nonroot:nonroot
+
+WORKDIR /app
+
+ENV VIRTUAL_ENV=/app/.venv \
+  PATH="/app/.venv/bin:$PATH"
+
+# COPY --from=build /usr/local/lib/python3.13/site-packages/ /usr/local/lib/python3.13/site-packages/
+# COPY --from=build /usr/local/bin/ /usr/local/bin/
+COPY --from=build --chown=nonroot:nonroot /app ./
+
+EXPOSE 8000
+
+CMD ["gunicorn", "--bind", "0.0.0.0:8000", "--workers", "3", "website.wsgi"]
--- website/settings.py
+++ website/settings.py
@@ -11,27 +11,32 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from storages.backends.s3boto3 import S3Boto3Storage
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATIC_ROOT = BASE_DIR / "static"
-STATIC_URL = "/static"
-STATICFILES_DIRS = [
-    BASE_DIR / "assets",
-]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-zwmgp!4y-@s#r0+v-3^d_e%)-h3&&w+6^a0e3mm!ajgln7r0&f"
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    default="django-insecure-zwmgp!4y-@s#r0+v-3^d_e%)-h3&&w+6^a0e3mm!ajgln7r0&f",
+)
+# SECRET_KEY = "django-insecure-zwmgp!4y-@s#r0+v-3^d_e%)-h3&&w+6^a0e3mm!ajgln7r0&f"
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get("DEBUG", default=1))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost").split(",")
 
+# AWS Settings
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 
 # Application definition
 
@@ -125,9 +130,28 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME")
+AWS_S3_FILE_OVERRITE = True
+AWS_DEFAULT_ACL = None
+AWS_S3_SIGNATURE_VERSION = "s3v4"
+AWES_QUERYSTARING_AUTH = False
+
+STATIC_ROOT = BASE_DIR / "static"
+STATIC_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/"
+STATICFILES_DIRS = [
+    BASE_DIR / "assets",
+]
+STORAGES = {
+    "default": {
+        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+    },
+    "staticfiles": {
+        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+    },
+}
